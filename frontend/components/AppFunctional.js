fbb8a15ddcc0637ecbc6a1ab62aa1b004c608338
semra-elcelik <semraelcelik@gmail.com>
2023-10-10 20:22:57 +0300

    State eklendi,koordinatlar takip ediliyor,reset, yönler alındı

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+// const initialMessage = "";
+// const initialEmail = "";
+// const initialSteps = 0;
+// const initialIndex = 4; //  "B" nin bulunduğu indexi
+const initialState = {
+  message: "",
+  email: "",
+  steps: 0,
+  index: 4,
+};
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+  const [state, setState] = useState(initialState);
 
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+    let x = (state.index % 3) + 1;
+    let y = Math.floor(state.index / 3) + 1;
+    return { x, y };
   }
 
   function getXYMesaj() {
@@ -23,12 +34,42 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setState(initialState);
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    if (yon === "left") {
+      setState({
+        ...state,
+        index: state.index - 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    } else if (yın === "right") {
+      setState({
+        ...state,
+        index: state.index + 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    } else if (yon === "up") {
+      setState({
+        ...state,
+        index: state.index - 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    } else if (yon === "down") {
+      setState({
+        ...state,
+        index: state.index + 1,
+        steps: state.steps + 1,
+        message: "",
+      });
+    }
   }
 
   function ilerle(evt) {
@@ -51,13 +92,11 @@ export default function AppFunctional(props) {
         <h3 id="steps">0 kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
+            {idx === 4 ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message"></h3>
@@ -74,5 +113,5 @@ export default function AppFunctional(props) {
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
